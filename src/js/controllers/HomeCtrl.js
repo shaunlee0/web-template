904c115572a230383a0226ebaa179f30cec460d7
Shaun Lee <shaunlee0@gmail.com>
2018-06-08 16:40:46 +0100

    Initial commit and add styles

--- src/js/controllers/HomeCtrl.js
+++ src/js/controllers/HomeCtrl.js
@@ -2,7 +2,11 @@ angular.module('templateApp')
     .controller('HomeCtrl', [
         '$scope',
         function($scope) {
+            var vm = this;
+
             console.log('Loaded.');
             $scope.message = 'Hello World Tests';
+
+            vm.message = 'Hello World Test';
         }
     ]);
